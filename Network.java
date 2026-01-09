57e417e9a0053f8ba2224c8713444ef18205640f
Noam Franchi <noam@Noams-MacBook-Air.local>
2026-01-09 19:14:47 +0200

    done2

--- Network.java
+++ Network.java
@@ -62,8 +62,7 @@ public class Network {
     public boolean addFollowee(String name1, String name2) {
         User user1 = getUser(name1);
         User user2 = getUser(name2);
-
-        if( user1 == null || user2 == null){
+        if(user1 == null || user2 == null || user1.equals(user2)){
             return false;
         }
         return user1.addFollowee(name2);
@@ -128,9 +127,9 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String ans = "Network:\n";
+        String ans = "Network:";
         for(int i = 0; i < userCount; i++){
-           ans += users[i].toString() + "\n";
+           ans += "\n" + users[i].toString();
         }
         return ans;
     }
